657f62f704c8d40cb344dd1cd692484d4a8761cc
thomas marquez <tmarquez@alaska.edu>
2018-05-14 20:23:09 -0800

    Lots of changes. Added Testimonial view, mondel, and view model for testimonial. Need to remember to checkin changes more. ugh.

--- KnikLandscaping/Controllers/HomeController.cs
+++ KnikLandscaping/Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace KnikLandscaping.Controllers
             return View();
         }
 
+        public IActionResult Testimonials()
+        {
+            ViewData["Message"] = "Testimonials";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
