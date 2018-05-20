cf5b9b2077879bc9663035998a7b1a29a4648311
thomas marquez <tmarquez@alaska.edu>
2018-05-20 15:42:53 -0800

    "Added a dbcontext

---
+++ KnikLandscaping/Models/KinkLandscapingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnikLandscaping.Models
+{
+    public class KinkLandscapingContext : DbContext
+    {
+        public KinkLandscapingContext()
+        {
+
+        }
+
+        public DbSet<Testimonials> Testimonials { get; set; }
+
+    }
+}
--- KnikLandscaping/Startup.cs
+++ KnikLandscaping/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KnikLandscaping.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,12 @@ namespace KnikLandscaping
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+
+            services.AddLogging();
+
+            services.AddDbContext<KinkLandscapingContext>();
+
             services.AddMvc();
         }
 
