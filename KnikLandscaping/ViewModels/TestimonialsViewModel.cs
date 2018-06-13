999829ac1608e2ca4f4bc770ca85b1c39be6726a
thomas marquez <tmarquez@alaska.edu>
2018-06-12 20:07:14 -0800

    changes to homecontroller, Testimonials view model and Testimonial view preparing for changes to view

--- KnikLandscaping/ViewModels/TestimonialsViewModel.cs
+++ KnikLandscaping/ViewModels/TestimonialsViewModel.cs
@@ -1,17 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using KnikLandscaping.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KnikLandscaping.ViewModels
 {
     public class TestimonialsViewModel
     {
-        public int ID{ get; set; }
+        //public int ID{ get; set; }
 
-        public string FirstName { get; set; }
+        //public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+        //public string LastName { get; set; }
 
-        [Required]
-        [StringLength(500, MinimumLength = 5)] //min length arbitrary...may need to change this
-        public string Content { get; set; }
+        //[Required]
+        //[StringLength(500, MinimumLength = 5)] //min length arbitrary...may need to change this
+        //public string Content { get; set; }
+
+        public List<Testimonial> testimonials { get; set; }
     }
 }
